be0f5d7c37e715e702d7252f035a1a902087b6e9
antsecv14 <38107769+antsecv14@users.noreply.github.com>
2018-05-31 11:44:29 -0500

    Update ClaseArreglos.java

--- ClaseArreglos/src/clasearreglos/ClaseArreglos.java
+++ ClaseArreglos/src/clasearreglos/ClaseArreglos.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class ClaseArreglos {
     public static void main(String[] args) {
         Scanner entrada=new Scanner (System.in);
+        //en la linea 9 se procede a inicializar lel arreglo 
         int[] c=new int[4];
+        //en la linea 11 se le da el valor de 10 al primer puesto del arreglo
         c[0]=10;
+        //el siguiente for es para presentar de arriba hacia abajo el arreglo
         for(int contador=0;contador<c.length;contador++){
             System.out.println(c[contador]);
         }
