6ac9cc59fa75715cf8a6183a974073c980535305
Antse Caceres <antsecv14@gmail.com>
2018-05-31 10:11:18 -0500

    trabajo entregado

---
+++ ClaseArreglos/src/clasearreglos/ClaseArreglos.java
@@ -0,0 +1,15 @@
+package clasearreglos;
+
+import java.util.Scanner;
+
+public class ClaseArreglos {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner (System.in);
+        int[] c=new int[4];
+        c[0]=10;
+        for(int contador=0;contador<c.length;contador++){
+            System.out.println(c[contador]);
+        }
+    }
+    
+}
